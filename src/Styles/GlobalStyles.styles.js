c2ecfee79271aec33d04a57ffd614d05fa0edf1f
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-07 01:22:00 +0900

    Revert "merge: feature/main 병합"
    This reverts commit 5c664cc00d536279d74099c8002489cb21ab5611, reversing
    changes made to b2c7e02da7258cc399429fddcba1a52b2f6eab8c.

--- src/Styles/GlobalStyles.styles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-// 전체 폰트 설정
-export const GlobalStyle = createGlobalStyle`
-    body {
-        font-family: "Pretendard Variable", -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif;
-        background-color: #F6F7F9;
-    }
-`;
