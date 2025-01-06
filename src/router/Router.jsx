51daa5966b796bccb3f3eee350d532ff380f0f94
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-06 15:51:01 +0900

    feat: activities 컴포넌트 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import AddInfo from "../pages/Login/AddInfo";
 import Login from "../pages/Login/Login";
+import Activities from "../pages/activities/activities";
 import Home from "../pages/home/Home";
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
                 <Route index element={<Home />} />
                 <Route path="/signup" element={<AddInfo />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="Activities" element={<Activities />} />
             </Route>
         </Routes>
     )
