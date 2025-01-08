22b71c2638a16cbc808e2f074f2dd16c55132d21
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-08 19:10:14 +0900

    refactor: Header 컴포넌트 navigate 로직 제거

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import * as S from "./Styles/Header.styles";
 
 const Header = () => {
-    const navigate = useNavigate();
-
-    const handleSignUp = () => {
-        navigate("/signup");
-    };
-
-    const handleLogin = () => {
-        navigate("/login");
-    };
-
     return (
         <S.HeaderContainer>
             <S.Logo to="/">Logo</S.Logo>
