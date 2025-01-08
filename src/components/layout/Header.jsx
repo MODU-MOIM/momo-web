e7ed22f3cabfa6a12864d46f0e5718d4c546853f
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-08 22:34:51 +0900

    feat: MyPage 컴포넌트 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import Mypage from "./MyPage";
+
+
 import * as S from "./Styles/Header.styles";
 
 const Header = () => {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const togglePopup = () => {
+        setIsPopupOpen(!isPopupOpen);
+    };
+
     return (
         <S.HeaderContainer>
             <S.Logo to="/">Logo</S.Logo>
@@ -12,6 +22,10 @@ const Header = () => {
                     <S.StyledNavLink>핫 플레이스</S.StyledNavLink>
                 </S.Nav>
                 <S.AuthButtons>
+                    <S.UserButton onClick={togglePopup}>
+                        {isPopupOpen && <Mypage />}
+                        <AiOutlineUser size={21} />
+                    </S.UserButton>
                     <S.StyledLoginLink to="/login">로그인</S.StyledLoginLink>
                     <S.StyledLoginLink to="/signup">회원가입</S.StyledLoginLink>
                 </S.AuthButtons>
