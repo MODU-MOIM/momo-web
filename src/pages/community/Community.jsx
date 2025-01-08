84fb97518743f901f4786f230de321b159f5105f
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-08 17:58:55 +0900

    feat: Community 페이지 생성

---
+++ src/pages/community/Community.jsx
@@ -0,0 +1,11 @@
+import * as S from "./Styles/Community.styles";
+
+const Community = () => {
+    return(
+        <>
+            <S.Container>123</S.Container>
+        </>
+    );
+}
+
+export default Community;
