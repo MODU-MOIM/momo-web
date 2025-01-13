383579a5c32fcc615e934d345dc7610f2f987f35
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-14 01:32:03 +0900

    feat: 로그인 상태에 따른 헤더 컴포넌트 UI 조건부 렌더링 구현

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
+import { useAuth } from "../../AuthProvider";
 import Mypage from "./MyPage";
-
-
 import * as S from "./Styles/Header.styles";
 
 const Header = () => {
     const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const { isLoggedIn, logout } = useAuth();
 
     const togglePopup = () => {
         setIsPopupOpen(!isPopupOpen);
@@ -27,12 +27,16 @@ const Header = () => {
                 </S.Nav>
                 <S.AuthButtons>
                     {/* 로그인 기능 구현 후 uid를 통해 uid가 있으면 userbutton출력 */}
-                    <S.UserButton onClick={togglePopup}>
-                        <AiOutlineUser size={21} />
-                    </S.UserButton>
-                    {/* uid가 없으면 로그인 회원가입 버튼 출력 */}
-                    <S.StyledLoginLink to="/login">로그인</S.StyledLoginLink>
-                    <S.StyledLoginLink to="/signup">회원가입</S.StyledLoginLink>
+                    {isLoggedIn ? (
+                        <S.UserButton onClick={togglePopup}>
+                            <AiOutlineUser size={21} />
+                        </S.UserButton>
+                    ) : (
+                        <>
+                            <S.StyledLoginLink to="/login">로그인</S.StyledLoginLink>
+                            <S.StyledLoginLink to="/signup">회원가입</S.StyledLoginLink>
+                        </>
+                    )}
                 </S.AuthButtons>
             </S.Container>
             {isPopupOpen && <Mypage closeModal={closeModal}/>}
