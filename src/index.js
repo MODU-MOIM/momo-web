69e99ed99340a593921c20e501b6060145055efc
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-14 01:30:25 +0900

    docs: AuthProvider 주석 추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { AuthProvider } from './AuthProvider';
 import Router from "./router/Router";
 import { GlobalStyle } from './Styles/GlobalStyles.styles';
 
+// AuthProvider를 최상위에 배치하여 모든 컴포넌트에서 인증 상태 접근 가능
+// AuthContext를 통해 로그인 상태, 토큰, 사용자 정보 등을 전역적으로 관리
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
