59179b8b11c37c8630eabfbf831f6be02edabbc8
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-14 01:27:21 +0900

    feat: AuthProvider 추가

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from './AuthProvider';
 import Router from "./router/Router";
 import { GlobalStyle } from './Styles/GlobalStyles.styles';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <AuthProvider>
       <BrowserRouter>
         <GlobalStyle />
         <Router />
       </BrowserRouter>
+      </AuthProvider>
   </React.StrictMode>
 );
