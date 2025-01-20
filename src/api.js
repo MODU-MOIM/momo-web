1c0b6a5d99a45d324850b6ed194ea2fd15c046b6
OhSSangHoon <gnsdkaos99@naver.com>
2025-01-20 22:24:53 +0900

    feat: - uploadProfileImage, updateUserInfo API 추가 - 파일 업로드를 위한 요청 인터셉터 수정 - reissue 제거

--- src/api.js
+++ src/api.js
@@ -1,65 +1,43 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:8080';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 });
 
 // 요청 인터셉터
 api.interceptors.request.use(
-    (config) => {
+    config => {
         const token = localStorage.getItem('token');
         if (token) {
-            config.headers.Authorization = token;
+            config.headers['Authorization'] = token;
+        }
+        // 파일 업로드 요청인 경우 Content-Type 헤더 제거
+        if (config.url === '/users/upload-profile') {
+            delete config.headers['Content-Type'];
         }
         return config;
     },
-    (error) => {
-        console.error('인터셉터 에러:', error);
-        return Promise.reject(error);
-    }
+    error => Promise.reject(error)
 );
 
-// 응답 인터셉터
-api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        console.error('응답 에러:', {
-            message: error.message,
-            response: error.response?.data,
-            status: error.response?.status
-        });
-        
-        if(error.response?.status === 401){
-            localStorage.clear();
-            window.location.href = '/';
-        }
-
-        return Promise.reject(error);
-    }
-);
-// API
 export const authAPI = {
-    // 회원가입
-    signUp: (data) => api.post('/auth/sign-up', data),
-    // 카테고리 저장
-    signUpCategory: (data) => api.post('/users/interests', data),
-    // 로그인
-    signIn: (data) => api.post('/auth/sign-in', data),
-    // 사용자 정보 조회
-    getUserInfo: () => api.get('/users/me'),
-    // 로그아웃
+    signUp: data => api.post('/auth/sign-up', data),
+    signUpCategory: data => api.post('/users/interests', data),
+    signIn: data => api.post('/auth/sign-in', data),
     signOut: () => api.post('/auth/sign-out'),
-    // 토큰 재발급
-    reissue: () => api.post('/auth/reissue'),
-    sendSms: (phoneNumber) => api.post('/auth/send-sms', { toPhoneNumber: phoneNumber }),
-    verifySms: (code) => api.post('/auth/verify-sms', { verificationCode: code }),
-    
+    getUserInfo: () => api.get('/users/me'),
+    sendSms: phoneNumber => api.post('/auth/send-sms', { toPhoneNumber: phoneNumber }),
+    verifySms: code => api.post('/auth/verify-sms', { verificationCode: code }),
+    uploadProfileImage: (formData) => api.post('/users/upload-profile', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    }),
+    updateUserInfo: (data) => api.put('/users', data),
 };
 
 export default api;
