d0a5c107d9ef8c812d94f4302aa71278488a0161
jihyun132 <jihyun.q.s@gmail.com>
2025-04-06 02:26:52 +0900

    test: 멤버리스트 팝업창에 띄우기

--- src/pages/CrewMain/Styles/Banner.styles.js
+++ src/pages/CrewMain/Styles/Banner.styles.js
@@ -102,8 +102,6 @@ export const Panel = styled.div`
 `;
 
 export const Container = styled.div`
-    /* top: 20%;
-    right: 15%; */
     width: 400px;
     height: 500px;
     background-color: white;
@@ -113,4 +111,69 @@ export const Container = styled.div`
     right: 25%;
     background: #fff;
     border-radius: 15px;
+`;
+
+export const MemberList = styled.div`
+    display: flex;
+    flex-direction: column;
+    width:100%;
+    gap: 10px;
+    max-height:500px;
+    overflow-y: auto;
+    padding: 50px;
+    overscroll-behavior: contain; // 스크롤 propagation 방지
+
+    &::-webkit-scrollbar {
+        width: 6px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: #f1f1f1;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: #888;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
+`;
+
+export const MemberItem = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 5px 0px;
+`;
+
+export const MemberImage = styled.img`
+    width:45px;
+    height:45px;
+    margin-right: 10px;
+    border:1px solid #797979;
+    border-radius: 50%;
+`;
+export const MemberName = styled.div`
+    margin: 0px 10px;
+    font-size: 15px;
+    font-weight: 600;
+`;
+export const MemberRole = styled.div`
+    color:#4B44B6;
+    font-size: 15px;
+    font-weight: 600;
+`;
+
+export const CloseButton = styled.div`
+    background: none;
+    border: none;
+    position: absolute;
+    display: flex;
+    top: 10px;
+    right: 10px;
+    z-index:2;
+    color: #999;
+    cursor: pointer;
 `;
--- src/pages/CrewMain/components/MemList.jsx
+++ src/pages/CrewMain/components/MemList.jsx
@@ -1,4 +1,17 @@
+import { AiOutlineClose } from "react-icons/ai";
 import * as S from "../Styles/Banner.styles";
+
+const profile = "https://yu-momo-bucket.s3.ap-northeast-2.amazonaws.com/profile/94b9c947-9324-4575-9980-ba2b15aa1124_testimg.png"
+const members = [
+    {id: 1, nickname: "짱구런", role: "리더", profileImage: profile},
+    {id: 2, nickname: "초보러닝", role: "멤버", profileImage: profile},
+    {id: 3, nickname: "루피피러닝", role: "관리자", profileImage: profile},
+    {id: 4, nickname: "헬로우", role: "멤버", profileImage: profile},
+    {id: 5, nickname: "짱구런", role: "리더", profileImage: profile},
+    {id: 6, nickname: "초보러닝", role: "멤버", profileImage: profile},
+    {id: 7, nickname: "루피피러닝", role: "관리자", profileImage: profile},
+    {id: 8, nickname: "헬로우", role: "멤버", profileImage: profile},
+];
 
 const MemList = ({ closeModal }) => {
     const handlePanelClick = (e) => {
@@ -10,6 +23,18 @@ const MemList = ({ closeModal }) => {
     return(
         <S.Panel onClick={handlePanelClick}>
             <S.Container>
+                <S.CloseButton onClick={() => closeModal()}>
+                    <AiOutlineClose size={24}/>
+                </S.CloseButton>
+                <S.MemberList>
+                    {members.map(member => (
+                        <S.MemberItem key={member.id}>
+                            <S.MemberImage src={member.profileImage}/>
+                            <S.MemberName>{member.nickname}</S.MemberName>
+                            <S.MemberRole>{member.role}</S.MemberRole>
+                        </S.MemberItem>
+                    ))}
+                </S.MemberList>
             </S.Container>
         </S.Panel>
     );
