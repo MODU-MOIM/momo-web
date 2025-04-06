a2b11311d1fce7643f49eb2711309f380b84a4cb
jihyun132 <jihyun.q.s@gmail.com>
2025-04-06 12:55:37 +0900

    feat: 배너 멤버 리스트 창 api 구현

--- src/pages/CrewMain/components/MemList.jsx
+++ src/pages/CrewMain/components/MemList.jsx
@@ -1,24 +1,31 @@
 import { AiOutlineClose } from "react-icons/ai";
 import * as S from "../Styles/Banner.styles";
-
-const profile = "https://yu-momo-bucket.s3.ap-northeast-2.amazonaws.com/profile/94b9c947-9324-4575-9980-ba2b15aa1124_testimg.png"
-const members = [
-    {id: 1, nickname: "짱구런", role: "리더", profileImage: profile},
-    {id: 2, nickname: "초보러닝", role: "멤버", profileImage: profile},
-    {id: 3, nickname: "루피피러닝", role: "관리자", profileImage: profile},
-    {id: 4, nickname: "헬로우", role: "멤버", profileImage: profile},
-    {id: 5, nickname: "짱구런", role: "리더", profileImage: profile},
-    {id: 6, nickname: "초보러닝", role: "멤버", profileImage: profile},
-    {id: 7, nickname: "루피피러닝", role: "관리자", profileImage: profile},
-    {id: 8, nickname: "헬로우", role: "멤버", profileImage: profile},
-];
+import { crewMembersAPI } from "../../../api";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const MemList = ({ closeModal }) => {
+    const { crewId } = useParams();
+    const [members, setMembers] = useState();
+
     const handlePanelClick = (e) => {
         if(e.target === e.currentTarget){
             closeModal();
         }
     }
+    const fetchMembers = async() => {
+        try {
+            const response = await crewMembersAPI.getMemberList(crewId);
+            console.log(response.data.data);
+            setMembers(response.data.data);
+        } catch (error) {
+            console.error("크루 멤버 읽기 실패", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchMembers();
+    },[]);
 
     return(
         <S.Panel onClick={handlePanelClick}>
@@ -27,8 +34,8 @@ const MemList = ({ closeModal }) => {
                     <AiOutlineClose size={24}/>
                 </S.CloseButton>
                 <S.MemberList>
-                    {members.map(member => (
-                        <S.MemberItem key={member.id}>
+                    {members?.map(member => (
+                        <S.MemberItem key={member.memberId}>
                             <S.MemberImage src={member.profileImage}/>
                             <S.MemberName>{member.nickname}</S.MemberName>
                             <S.MemberRole>{member.role}</S.MemberRole>
