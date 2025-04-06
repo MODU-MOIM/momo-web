c8002a9fce1699d0b9150e9b6b2b0e798185cb45
jihyun132 <jihyun.q.s@gmail.com>
2025-04-06 15:27:01 +0900

    feat: 멤버 평가 페이지 멤버리스트 api 구현

--- src/pages/Review/components/Member.jsx
+++ src/pages/Review/components/Member.jsx
@@ -1,21 +1,28 @@
+import { useParams } from "react-router-dom";
+import { crewMembersAPI } from "../../../api";
 import * as S from "../Styles/Review.styles";
-
-const profile = "https://yu-momo-bucket.s3.ap-northeast-2.amazonaws.com/profile/94b9c947-9324-4575-9980-ba2b15aa1124_testimg.png"
-const members = [
-    {id: 1, nickname: "짱구런", role: "리더", profileImage: profile},
-    {id: 2, nickname: "초보러닝", role: "멤버", profileImage: profile},
-    {id: 3, nickname: "루피피러닝", role: "관리자", profileImage: profile},
-    {id: 4, nickname: "헬로우", role: "멤버", profileImage: profile},
-    {id: 5, nickname: "짱구런", role: "리더", profileImage: profile},
-    {id: 6, nickname: "초보러닝", role: "멤버", profileImage: profile},
-    {id: 7, nickname: "루피피러닝", role: "관리자", profileImage: profile},
-    {id: 8, nickname: "헬로우", role: "멤버", profileImage: profile},
-];
+import { useEffect, useState } from "react";
 
 export default function Member() {
+    const { crewId } = useParams();
+    const [members, setMembers] = useState();
+
+    const fetchMembers = async() => {
+        try {
+            const response = await crewMembersAPI.getMemberList(crewId);
+            setMembers(response.data.data);
+        } catch (error) {
+            console.error("크루 멤버 읽기 실패", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchMembers();
+    },[]);
+
     return (
         <S.Wrapper>
-            {members.map(mem => (
+            {members?.map(mem => (
                 <S.MemReviewItem>
                     <S.MemProfile src={mem.profileImage}/>
                     <S.ContainerWrapper>
