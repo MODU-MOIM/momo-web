39630a26f6098f6150a93b6c84f58a2ba41bffef
jihyun132 <jihyun.q.s@gmail.com>
2025-04-06 12:48:38 +0900

    design: 멤버 평가페이지 별점 제외 퍼블리싱 완료

--- src/pages/Review/components/Member.jsx
+++ src/pages/Review/components/Member.jsx
@@ -1,7 +1,39 @@
+import * as S from "../Styles/Review.styles";
+
+const profile = "https://yu-momo-bucket.s3.ap-northeast-2.amazonaws.com/profile/94b9c947-9324-4575-9980-ba2b15aa1124_testimg.png"
+const members = [
+    {id: 1, nickname: "짱구런", role: "리더", profileImage: profile},
+    {id: 2, nickname: "초보러닝", role: "멤버", profileImage: profile},
+    {id: 3, nickname: "루피피러닝", role: "관리자", profileImage: profile},
+    {id: 4, nickname: "헬로우", role: "멤버", profileImage: profile},
+    {id: 5, nickname: "짱구런", role: "리더", profileImage: profile},
+    {id: 6, nickname: "초보러닝", role: "멤버", profileImage: profile},
+    {id: 7, nickname: "루피피러닝", role: "관리자", profileImage: profile},
+    {id: 8, nickname: "헬로우", role: "멤버", profileImage: profile},
+];
+
 export default function Member() {
     return (
-        <div>
-            <h1>멤버리뷰</h1>
-        </div>
+        <S.Wrapper>
+            {members.map(mem => (
+                <S.MemReviewItem>
+                    <S.MemProfile src={mem.profileImage}/>
+                    <S.ContainerWrapper>
+                        {/* 위쪽 컨테이너 */}
+                        <S.MRTopContainer>
+                            <S.MemName>{mem.nickname}</S.MemName>
+                            <S.StarReview>12345</S.StarReview>
+                        </S.MRTopContainer>
+                        {/* 아래쪽 컨테이너 */}
+                        <S.MRBottomContainer>
+                            <S.SingleLineReview
+                                placeholder="한 줄 평가"
+                            />
+                            <S.SubmitButton>저장</S.SubmitButton>
+                        </S.MRBottomContainer>
+                    </S.ContainerWrapper>
+                </S.MemReviewItem>
+            ))}
+        </S.Wrapper>
     );
 }
